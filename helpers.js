33dc1cfb398bf34d25d240097ddd250a38b2916b
Ken Marsh <10334573+kenskyfish@users.noreply.github.com>
2023-01-16 10:38:39 +0100

    [KP-1036] assorted fixes and enhancements

--- helpers.js
+++ helpers.js
@@ -1,19 +1,41 @@
 const util = require("util");
 const childProcess = require("child_process");
-const { access } = require("fs/promises");
-const fs = require("fs");
+const fs = require("fs/promises");
 
 const exec = util.promisify(childProcess.exec);
 
-async function assertPathExistence(path) {
+async function statPath(path) {
+  let stats;
   try {
-    await access(path, fs.constants.F_OK);
+    stats = await fs.stat(path);
+    if (stats.isFile()) {
+      return "FILE";
+    }
+    if (stats.isDirectory()) {
+      return "DIRECTORY";
+    }
   } catch {
-    throw new Error(`Path ${path} does not exist`);
+    return "DOES_NOT_EXIST";
+  }
+  return "UNKNOWN";
+}
+
+async function getFileContent({ PATH: path }) {
+  const stat = await statPath(path);
+  if (stat === "FILE") {
+    try {
+      return await fs.readFile(path, { encoding: "utf8" });
+    } catch (error) {
+      throw new Error(`Failed to read content of a file at ${path}: ${error.message || JSON.stringify(error)}`);
+    }
+  } else {
+    console.error(`No file was found at ${path}`);
+    return undefined;
   }
 }
 
 module.exports = {
-  assertPathExistence,
+  statPath,
   exec,
+  getFileContent,
 };
